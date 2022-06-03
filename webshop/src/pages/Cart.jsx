73765bdd3b85059e9e80744b7f95edef4e2959d4
mihkelvaher <mihkel.vaher@industry62.com>
2022-06-03 08:59:56 +0300

    kogused ostukorvis, andmebaas, ostukorvi kujundus

--- webshop/src/pages/Cart.jsx
+++ webshop/src/pages/Cart.jsx
@@ -1,6 +1,64 @@
+import { useState } from "react";
+import "../css/cart.css";
 
 function Cart() {
-  return (<div>Cart</div>)
+  const [cartProducts, setCartProducts] = useState(
+        JSON.parse(sessionStorage.getItem("cartProducts")) || []
+    );
+
+  const decreaseQuantity = (productClicked) => {
+    const index = cartProducts.findIndex(element => element.product.id === productClicked.product.id);
+    cartProducts[index].quantity = cartProducts[index].quantity - 1;
+    if (cartProducts[index].quantity === 0) {
+      removeFromCart(productClicked);
+    }
+    setCartProducts(cartProducts.slice());
+    sessionStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+  }
+
+  const increaseQuantity = (productClicked) => {
+    const index = cartProducts.findIndex(element => element.product.id === productClicked.product.id);
+    cartProducts[index].quantity = cartProducts[index].quantity + 1;
+    setCartProducts(cartProducts.slice());
+    sessionStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+  }
+
+  const removeFromCart = (productClicked) => { // MOZILLA --> findIndex()
+    const index = cartProducts.find(element => element.product.id === productClicked.product.id);
+    cartProducts.splice(index,1);
+    setCartProducts(cartProducts.slice());
+    sessionStorage.setItem("cartProducts", JSON.stringify(cartProducts));
+  }
+
+
+              // [{product: {TOODE}, quantity: 1}, {product: {TOODE}, quantity: 5}]
+  return (<div>
+    <button>Tühjenda --- KOJU</button>
+    { cartProducts.map(element => 
+    <div className="cartProduct">
+      <img className="cartProductImg" src={element.product.imgSrc} alt="" />
+      <div className="cartProductName">{element.product.name}</div>
+      <div className="cartProductPrice">{element.product.price} €</div>
+      <div className="cartProductQuantity">
+        <img className="cartProductButton" 
+             onClick={() => decreaseQuantity(element)} 
+             src={require('../assets/minus.png')}
+             alt="" />
+        <div>{element.quantity} tk</div>
+        <img className="cartProductButton" 
+             onClick={() => increaseQuantity(element)} 
+             src={require('../assets/plus.png')}
+             alt="" />
+      </div>
+      <div className="cartProductTotal">{element.product.price * element.quantity} €</div>
+      <img className="cartProductButton" 
+             onClick={() => removeFromCart(element)} 
+             src={require('../assets/delete.png')}
+             alt="" />
+      <br />
+    </div>
+    ) }
+    </div>)
 }
 
 export default Cart;
--- webshop/src/pages/Home.jsx
+++ webshop/src/pages/Home.jsx
@@ -1,6 +1,54 @@
+import { useEffect, useState } from "react";
 
 function Home() {
-  return (<div>Home</div>)
+  const productsUrl = "https://react-5-2022-default-rtdb.europe-west1.firebasedatabase.app/products.json";
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    fetch(productsUrl)
+    .then(res => res.json()) // response (tagastus) - kogu info
+    .then(body => {   // sisu mis kaasa tuleb kogu infost
+      // body === {-asdeqdasd: {TOODE}}   --> [{TOODE}]
+      const newArray = [];
+      for (const key in body) {
+        newArray.push(body[key])
+      }
+      setProducts(newArray);
+    })
+  },[]);
+  
+
+  // [{nimi: "Coca", price: 5}, {nimi: "Coca", price: 5}, {nimi: "Coca", price: 5}]
+  // [{toode: {id: 1238, nimi: "Coca", price: 5}, kogus: 1}]
+  // objekti sisse tekib teine objekt
+  //            {id: 1238, nimi: "Coca", price: 5}
+  const addToCart = (productClicked) => {
+    let cProducts = sessionStorage.getItem("cartProducts");
+    cProducts = JSON.parse(cProducts) || [];
+    const index = cProducts.findIndex(element => element.product.id === productClicked.id);
+    if (index >= 0) {
+      cProducts[index].quantity = cProducts[index].quantity + 1;
+    } else {
+      cProducts.push({"product": productClicked, "quantity": 1});
+    }
+    cProducts = JSON.stringify(cProducts);
+    sessionStorage.setItem("cartProducts", cProducts);
+  }
+
+  // 1. sessionStorage.getItem("") <--- võtan sessionStorage-st
+  // 2. JSON.parse() <--- Võtan jutumärgid maha 
+  // 3. .push()   <--- lisan juurde
+  // 4. JSON.stringify()    <--- pean massiivi tegema jutumärkide kujule
+  // 5. sessionStorage.setItem("VÕTI", UUED_TOOTED)   <--- pean panema sessionstorage-sse
+
+  return (<div>{ products.map(element => 
+    <div>
+      <img src={element.imgSrc} alt="" />
+      <div>{element.name}</div>
+      <div>{element.price}</div>
+      <button onClick={() => addToCart(element)}>Lisa ostukorvi</button>
+    </div>) 
+    }</div>)
 }
 
 export default Home;
